65dd7d134d2eb171ce440111eba50efa62f5df33
Schor <beto@brius.com.br>
2024-02-25 19:58:22 -0300

    Can generate report from books file

---
+++ main.py
@@ -0,0 +1,37 @@
+import os
+
+# Get the current working directory
+cwd = os.getcwd()
+
+# Get txt files from ./books/ directory
+txt_files = [file for file in os.listdir('books') if file.endswith('.txt')]
+
+def open_file(file):
+    with open(f'books/{file}', 'r') as file:
+        book_text = file.read()
+        number_of_characters = len(book_text.split())
+        return book_text, number_of_characters
+
+def count_chars(text):
+    char_dict = {}
+    chars = sorted(list(set(text.lower())))
+    for char in chars:
+        if char.isalpha():
+            char_dict[char] = text.lower().count(char)
+    return len(chars), char_dict
+
+def build_report_text(text):
+    num_chars, char_dict = count_chars(text)
+    report_text = f'The text contains {num_chars} unique characters.\n'
+    ordered_list = reversed(sorted(char_dict.items(), key=lambda x:x[1]))
+    for char in ordered_list:
+        report_text += f"The '{char[0]}' character was found {char[1]} times\n"
+    return report_text
+
+if __name__ == '__main__':
+    for file in txt_files:
+        book_text, number_of_characters = open_file(file)
+        report_text = build_report_text(book_text)
+        print(f'{file.split('.')[0]} contains {number_of_characters} words.')
+        print(report_text)
+        print('\n')
